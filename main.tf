ab00a1c03397ffdcf6316feb41880781818c3186
mounika konda <konda2mounika@gmail.com>
2023-10-06 15:13:19 +0530

    0001k8s

--- main.tf
+++ main.tf
@@ -38,3 +38,53 @@ resource "aws_route_table_association" "rta2" {
   subnet_id      = aws_subnet.sub2.id
   route_table_id = aws_route_table.RT.id
 }
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "abhisheksterraform2023project"
+}
+
+
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata1.sh"))
+}
